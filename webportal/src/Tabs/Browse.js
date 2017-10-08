6c2ddc79807de8e12abecbf442d31768e14524ad
Sergei Chestakov <sergei332@gmail.com>
2017-10-07 18:44:12 -0700

    Search functionality in Browse tab

--- webportal/src/Tabs/Browse.js
+++ webportal/src/Tabs/Browse.js
@@ -30,6 +30,9 @@ export default class BrowseView extends Component {
     super(props);
 
     this.state = {
+      searchString: '',
+      searchFocusIndex: 0,
+      searchFoundCount: null,
       treeData: [
           { title: 'Category', children: [
             { title: 'Politics' },
@@ -48,11 +51,84 @@ export default class BrowseView extends Component {
   }
 
   render() {
+
+    const { searchString, searchFocusIndex, searchFoundCount } = this.state;
+
+   // Case insensitive search of `node.title`
+   const customSearchMethod = ({ node, searchQuery }) =>
+     searchQuery &&
+     node.title.toLowerCase().indexOf(searchQuery.toLowerCase()) > -1;
+
+   const selectPrevMatch = () =>
+     this.setState({
+       searchFocusIndex:
+         searchFocusIndex !== null
+           ? (searchFoundCount + searchFocusIndex - 1) % searchFoundCount
+           : searchFoundCount - 1,
+     });
+
+   const selectNextMatch = () =>
+     this.setState({
+       searchFocusIndex:
+         searchFocusIndex !== null
+           ? (searchFocusIndex + 1) % searchFoundCount
+           : 0,
+     });
+
     return (
       <div style={{ height: 1000 }}>
+        <form
+            style={{ display: 'inline-block' }}
+            onSubmit={event => {
+              event.preventDefault();
+            }}
+          >
+            <input
+              id="find-box"
+              type="text"
+              placeholder="Search..."
+              style={{ fontSize: '1rem' }}
+              value={searchString}
+              onChange={event =>
+                this.setState({ searchString: event.target.value })}
+            />
+
+            <button
+              type="button"
+              disabled={!searchFoundCount}
+              onClick={selectPrevMatch}
+            >
+              &lt;
+            </button>
+
+            <button
+              type="submit"
+              disabled={!searchFoundCount}
+              onClick={selectNextMatch}
+            >
+              &gt;
+            </button>
+
+            <span>
+              &nbsp;
+              {searchFoundCount > 0 ? searchFocusIndex + 1 : 0}
+              &nbsp;/&nbsp;
+              {searchFoundCount || 0}
+            </span>
+          </form>
         <SortableTree
           treeData={this.state.treeData}
           onChange={treeData => this.setState({ treeData })}
+          //Search methods
+            searchMethod={customSearchMethod}
+            searchQuery={searchString}
+            searchFocusOffset={searchFocusIndex}
+            searchFinishCallback={matches =>
+              this.setState({
+                searchFoundCount: matches.length,
+                searchFocusIndex:
+                  matches.length > 0 ? searchFocusIndex % matches.length : 0,
+              })}
         />
       </div>
     );
