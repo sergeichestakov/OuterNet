4e035c67510d04751a0255efc9eb0adc344d0ba3
Sergei Chestakov <sergei332@gmail.com>
2017-10-07 18:46:25 -0700

    Removed search tab

--- webportal/src/Content.js
+++ webportal/src/Content.js
@@ -2,23 +2,20 @@ import React, { Component } from 'react';
 import { Tab, Tabs } from 'react-mdl';
 import BrowseView from './Tabs/Browse.js';
 import RecentView from './Tabs/Recent.js';
-import SearchView from './Tabs/Search.js';
 import './App.css';
 
 export default class TabView extends Component {
 
   constructor(props) {
       super(props)
-      this.state = { activeTab: 1 };
+      this.state = { activeTab: 0 };
   }
 
   renderContent(index){
       if(index === 0){
         return(<RecentView />);
-      } else if (index === 1){
-        return(<BrowseView />);
       } else {
-        return(<SearchView />);
+        return(<BrowseView />);
       }
   }
 
@@ -28,7 +25,6 @@ export default class TabView extends Component {
         <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
             <Tab>Recent Articles</Tab>
             <Tab>Browse</Tab>
-            <Tab>Search</Tab>
         </Tabs>
         <section>
             <div className="content">{this.renderContent(this.state.activeTab)}</div>
